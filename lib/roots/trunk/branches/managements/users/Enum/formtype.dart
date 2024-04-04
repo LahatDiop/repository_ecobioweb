2ff871894fdba8368d2256d91d09913fc44f995c
Lahat <lahat.diop@inrebus.it>
2024-04-04 12:30:48 +0200

    Change language default lang app and setting_screen

---
+++ lib/roots/trunk/branches/managements/users/Enum/formtype.dart
@@ -0,0 +1,16 @@
+enum FormType {
+  Text,
+  Multiline,
+  Dropdown,
+  DropdownRole,
+  DropdownMarket,
+  DropdownMarketsEnabled,
+  AutoComplete,
+  HTMLReader,
+  DatePicker,
+  // CheckBox,
+  RollingSwitch, /// ON OFF
+  RollingSwitchList,
+  // SizeBoxedRoles,
+  //Flag
+}
---
+++ lib/roots/trunk/branches/managements/users/Enum/validator_type.dart
@@ -0,0 +1,1 @@
+enum validatorType { Notempty,isEnabled, TextLength, PhoneNumber, Age, Email, Market }
